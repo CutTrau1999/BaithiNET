2c63e9b9554651ff5ddc84e32b7148c8bb8c8de1
Tuan Nguyen <famouspeople8k@gmail.com>
2021-09-01 13:09:49 +0700

    Add project files.

---
+++ Models/BaiThiASPContext.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BaithiNET.Models
+{
+    public partial class BaiThiASPContext : DbContext
+    {
+        public BaiThiASPContext()
+        {
+        }
+
+        public BaiThiASPContext(DbContextOptions<BaiThiASPContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Classroom> Classrooms { get; set; }
+        public virtual DbSet<Exam> Exams { get; set; }
+        public virtual DbSet<Falculty> Falculties { get; set; }
+        public virtual DbSet<MigrationHistory> MigrationHistories { get; set; }
+        public virtual DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-LU1MKI5\\SQLEXPRESS;Initial Catalog=BaiThiASP;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Classroom>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Exam>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Falculty>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<MigrationHistory>(entity =>
+            {
+                entity.HasKey(e => new { e.MigrationId, e.ContextKey })
+                    .HasName("PK_dbo.__MigrationHistory");
+
+                entity.ToTable("__MigrationHistory");
+
+                entity.Property(e => e.MigrationId).HasMaxLength(150);
+
+                entity.Property(e => e.ContextKey).HasMaxLength(300);
+
+                entity.Property(e => e.Model).IsRequired();
+
+                entity.Property(e => e.ProductVersion)
+                    .IsRequired()
+                    .HasMaxLength(32);
+            });
+
+            modelBuilder.Entity<Subject>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Exam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BaithiNET.Models
+{
+    public partial class Exam
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StartTime { get; set; }
+        public string Date { get; set; }
+        public string Duration { get; set; }
+        public string Classroom { get; set; }
+        public string Faculty { get; set; }
+        public string Status { get; set; }
+    }
+}
